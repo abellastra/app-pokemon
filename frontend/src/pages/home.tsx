f3d080d8c48b23a315f55855e4707daa234d3b6e
gianella lastra <gianellalastra4@gmail.com>
2025-07-29 14:56:38 -0300

    maquetacion de home

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,7 +1,118 @@
-function home ()
+function Home ()
 {
     return(
-        <h1>home</h1>
+        <>
+        <header className="">
+            <h1 className=" flex justify-center text-center bg-gradient-to-b from-red-500 via-green-500 to-blue-500 bg-clip-text text-transparent font-bold text-4xl mt-5">Pokemones</h1>
+
+        <main className="pt-[50vh]">
+            <div className="grid grid-cols-5 grid-rows-3 gap-4 ">
+                <a href="https://www.youtube.com/watch?v=7iobxzd_2wY&list=PLUofhDIg_38q4D0xNWp7FEHOTcZhjWJ29" className="relative rounded-3xl overflow-hidden
+                bg-gradient-to-br from-cyan-950 via-cyan-700
+                 to-cyan-500
+                 border border-cyan-900
+                 flex flex-col items-start
+                 hover:scale-105 hover:contrast-125
+                 transition
+                 col-span-1 p-7  min-h-[180px]
+                 group">
+                    <span className="absolute bottom-2 left-0 font-bold text-2xl"></span>
+
+                    <svg  width="100" height="100" className="absolute -bottom-2 -right-2 group-hover:rotate-12 transition-transform ">
+                        
+                    </svg>
+                    <span className="absolute top-2 left-2 text-xs border rounded-xl px-2 py-0.5"></span>
+                </a>
+
+                <a href="https://www.youtube.com/watch?v=3nYLTiY5skU" className="relative rounded-3xl overflow-hidden
+                bg-gradient-to-br from-amber-900 via-amber-700
+                 to-amber-500
+                 border border-amber-900
+                 flex flex-col items-start
+                 hover:scale-105 hover:contrast-125
+                 transition
+                 col-span-1 p-7  min-h-[180px]
+                 group">
+                    <span className="absolute bottom-2 left-0 font-bold text-2xl"></span>
+
+                    <svg  width="100" height="100" className="absolute -bottom-2 -right-2 -rotate-6 size-28 group-hover:rotate-12 transition-transform ">
+                       
+                    </svg>
+                    <span className="absolute top-2 left-2 text-xs border rounded-xl px-2 py-0.5"></span>
+                </a>
+
+                <a href="https://www.youtube.com/watch?v=TlJbu0BMLaY" className="relative rounded-3xl overflow-hidden
+                bg-gradient-to-br from-fuchsia-950  via-fuchsia-700
+                 to-fuchsia-500
+                 border border-fuchsia-900
+                 flex flex-col items-start
+                 hover:scale-105 hover:contrast-125
+                 transition
+                 col-span-1 p-7  min-h-[180px]
+                 group">
+                    <span className="absolute bottom-2 left-0 font-bold text-2xl"></span>
+
+                    <svg  width="100" height="100" className="absolute -bottom-2 -right-2 -rotate-6 size-28 group-hover:rotate-12 transition-transform ">
+                       
+                    </svg>
+                    <span className="absolute top-2 left-2 text-xs border rounded-xl px-2 py-0.5"></span>
+                </a>
+
+                <a href="https://www.youtube.com/watch?v=yB4n_K7dZV8&list=PLUofhDIg_38qm2oPOV-IRTTEKyrVBBaU7" className="relative rounded-3xl overflow-hidden
+                bg-gradient-to-br from-green-900 via-green-700
+                 to-green-500
+                 border border-green-900
+                 flex flex-col items-start
+                 hover:scale-105 hover:contrast-125
+                 transition
+                 col-span-1 p-7  min-h-[180px]
+                 group">
+                    <span className="absolute bottom-2 left-0 font-bold text-2xl"></span>
+
+                    <svg  width="100" height="100" className="absolute -bottom-2 -right-2 -rotate-6 size-28 group-hover:rotate-12 transition-transform ">
+                        
+                    </svg>
+                    <span className="absolute top-2 left-2 text-xs border rounded-xl px-2 py-0.5"></span>
+                </a>
+
+                <a href="https://www.youtube.com/watch?v=3nYLTiY5skU" className="relative rounded-3xl overflow-hidden
+                bg-gradient-to-br from-amber-900 via-amber-700
+                 to-amber-500
+                 border border-amber-900
+                 flex flex-col items-start
+                 hover:scale-105 hover:contrast-125
+                 transition
+                 col-span-1 p-7  min-h-[180px]
+                 group">
+                    <span className="absolute bottom-2 left-0 font-bold text-2xl"></span>
+
+                    <svg  width="100" height="100" className="absolute -bottom-2 -right-2 -rotate-6 size-28 group-hover:rotate-12 transition-transform ">
+                       
+                    </svg>
+                    <span className="absolute top-2 left-2 text-xs border rounded-xl px-2 py-0.5"></span>
+                </a>
+                <a href="https://www.youtube.com/watch?v=TlJbu0BMLaY" className="relative rounded-3xl overflow-hidden
+                bg-gradient-to-br from-fuchsia-950  via-fuchsia-700
+                 to-fuchsia-500
+                 border border-fuchsia-900
+                 flex flex-col items-start
+                 hover:scale-105 hover:contrast-125
+                 transition
+                 col-span-1 p-7  min-h-[180px]
+                 group">
+                    <span className="absolute bottom-2 left-0 font-bold text-2xl"></span>
+
+                    <svg  width="100" height="100" className="absolute -bottom-2 -right-2 -rotate-6 size-28 group-hover:rotate-12 transition-transform ">
+                       
+                    </svg>
+                    <span className="absolute top-2 left-2 text-xs border rounded-xl px-2 py-0.5"></span>
+                </a>
+            </div>
+        </main>
+        
+        </header>
+        </>
     )
+    
 }
-export default home
+export default Home
