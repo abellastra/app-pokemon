26bec36e0b2e6efe5218a8b5b0c662be4e08cdb2
gianella lastra <gianellalastra4@gmail.com>
2025-07-31 01:00:57 -0300

    Diseñé el layout de las tarjetas de Pokémon y preparé el fetch que pide los datos

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,113 +1,32 @@
-function Home ()
-{
+function Home (){
+    const pedirDatosPokemones = async () => {
+        const response: Response = await fetch("http://localhost:3000/datosPokemones",{
+            method: "GET",
+            headers: {
+                "content-type": "aplication-json"
+            },
+            body: JSON.stringify({}),
+        })
+        if(response.ok){
+            const data = response.json()
+            console.log(data)
+            // aca tendria que navegar a las tarjetas y pasarle los datos para 
+            // llenarlas
+        }
+    }
     return(
         <>
         <header className="">
             <h1 className=" flex justify-center text-center bg-gradient-to-b from-red-500 via-green-500 to-blue-500 bg-clip-text text-transparent font-bold text-4xl mt-5">Pokemones</h1>
 
-        <main className="pt-[50vh]">
-            <div className="grid grid-cols-5 grid-rows-3 gap-4 ">
-                <a href="https://www.youtube.com/watch?v=7iobxzd_2wY&list=PLUofhDIg_38q4D0xNWp7FEHOTcZhjWJ29" className="relative rounded-3xl overflow-hidden
-                bg-gradient-to-br from-cyan-950 via-cyan-700
-                 to-cyan-500
-                 border border-cyan-900
-                 flex flex-col items-start
-                 hover:scale-105 hover:contrast-125
-                 transition
-                 col-span-1 p-7  min-h-[180px]
-                 group">
-                    <span className="absolute bottom-2 left-0 font-bold text-2xl"></span>
+    
+        <main className="py-4 px-4 pb-16 max-w-xl mx-auto min-h-dvh mt-14">
+            
+           
+         <button className="w-36 bg-amber-400 border border-black rounded-3xl 
+  flex justify-center mx-auto" onClick={() => pedirDatosPokemones}>Generar</button>
+        
 
-                    <svg  width="100" height="100" className="absolute -bottom-2 -right-2 group-hover:rotate-12 transition-transform ">
-                        
-                    </svg>
-                    <span className="absolute top-2 left-2 text-xs border rounded-xl px-2 py-0.5"></span>
-                </a>
-
-                <a href="https://www.youtube.com/watch?v=3nYLTiY5skU" className="relative rounded-3xl overflow-hidden
-                bg-gradient-to-br from-amber-900 via-amber-700
-                 to-amber-500
-                 border border-amber-900
-                 flex flex-col items-start
-                 hover:scale-105 hover:contrast-125
-                 transition
-                 col-span-1 p-7  min-h-[180px]
-                 group">
-                    <span className="absolute bottom-2 left-0 font-bold text-2xl"></span>
-
-                    <svg  width="100" height="100" className="absolute -bottom-2 -right-2 -rotate-6 size-28 group-hover:rotate-12 transition-transform ">
-                       
-                    </svg>
-                    <span className="absolute top-2 left-2 text-xs border rounded-xl px-2 py-0.5"></span>
-                </a>
-
-                <a href="https://www.youtube.com/watch?v=TlJbu0BMLaY" className="relative rounded-3xl overflow-hidden
-                bg-gradient-to-br from-fuchsia-950  via-fuchsia-700
-                 to-fuchsia-500
-                 border border-fuchsia-900
-                 flex flex-col items-start
-                 hover:scale-105 hover:contrast-125
-                 transition
-                 col-span-1 p-7  min-h-[180px]
-                 group">
-                    <span className="absolute bottom-2 left-0 font-bold text-2xl"></span>
-
-                    <svg  width="100" height="100" className="absolute -bottom-2 -right-2 -rotate-6 size-28 group-hover:rotate-12 transition-transform ">
-                       
-                    </svg>
-                    <span className="absolute top-2 left-2 text-xs border rounded-xl px-2 py-0.5"></span>
-                </a>
-
-                <a href="https://www.youtube.com/watch?v=yB4n_K7dZV8&list=PLUofhDIg_38qm2oPOV-IRTTEKyrVBBaU7" className="relative rounded-3xl overflow-hidden
-                bg-gradient-to-br from-green-900 via-green-700
-                 to-green-500
-                 border border-green-900
-                 flex flex-col items-start
-                 hover:scale-105 hover:contrast-125
-                 transition
-                 col-span-1 p-7  min-h-[180px]
-                 group">
-                    <span className="absolute bottom-2 left-0 font-bold text-2xl"></span>
-
-                    <svg  width="100" height="100" className="absolute -bottom-2 -right-2 -rotate-6 size-28 group-hover:rotate-12 transition-transform ">
-                        
-                    </svg>
-                    <span className="absolute top-2 left-2 text-xs border rounded-xl px-2 py-0.5"></span>
-                </a>
-
-                <a href="https://www.youtube.com/watch?v=3nYLTiY5skU" className="relative rounded-3xl overflow-hidden
-                bg-gradient-to-br from-amber-900 via-amber-700
-                 to-amber-500
-                 border border-amber-900
-                 flex flex-col items-start
-                 hover:scale-105 hover:contrast-125
-                 transition
-                 col-span-1 p-7  min-h-[180px]
-                 group">
-                    <span className="absolute bottom-2 left-0 font-bold text-2xl"></span>
-
-                    <svg  width="100" height="100" className="absolute -bottom-2 -right-2 -rotate-6 size-28 group-hover:rotate-12 transition-transform ">
-                       
-                    </svg>
-                    <span className="absolute top-2 left-2 text-xs border rounded-xl px-2 py-0.5"></span>
-                </a>
-                <a href="https://www.youtube.com/watch?v=TlJbu0BMLaY" className="relative rounded-3xl overflow-hidden
-                bg-gradient-to-br from-fuchsia-950  via-fuchsia-700
-                 to-fuchsia-500
-                 border border-fuchsia-900
-                 flex flex-col items-start
-                 hover:scale-105 hover:contrast-125
-                 transition
-                 col-span-1 p-7  min-h-[180px]
-                 group">
-                    <span className="absolute bottom-2 left-0 font-bold text-2xl"></span>
-
-                    <svg  width="100" height="100" className="absolute -bottom-2 -right-2 -rotate-6 size-28 group-hover:rotate-12 transition-transform ">
-                       
-                    </svg>
-                    <span className="absolute top-2 left-2 text-xs border rounded-xl px-2 py-0.5"></span>
-                </a>
-            </div>
         </main>
         
         </header>
---
+++ frontend/src/pages/pokemones.tsx
@@ -0,0 +1,65 @@
+function Pokemones(){
+    return(
+         <div className="grid grid-cols-3 grid-rows-2 gap-2 w-full">
+                <a className="relative rounded-3xl overflow-hidden
+                bg-gradient-to-br from-cyan-950 via-cyan-700
+                 to-cyan-500
+                 border border-cyan-900
+                 flex flex-col items-start
+                 hover:scale-105 hover:contrast-125
+                 transition
+                 col-span-1 p-7  min-h-[180px]
+                 group">
+                    
+                </a>
+
+                <a className="relative rounded-3xl overflow-hidden
+                bg-gradient-to-br from-amber-900 via-amber-700
+                 to-amber-500
+                 border border-amber-900
+                 flex flex-col items-start
+                 hover:scale-105 hover:contrast-125
+                 transition
+                 col-span-1 p-7  min-h-[180px]
+                 group">
+                    
+                </a>
+                <a className="relative rounded-3xl overflow-hidden
+                bg-gradient-to-br from-amber-900 via-amber-700
+                 to-amber-500
+                 border border-amber-900
+                 flex flex-col items-start
+                 hover:scale-105 hover:contrast-125
+                 transition
+                 col-span-1 p-7  min-h-[180px]
+                 group">
+                    
+                </a>
+
+                <a className="relative rounded-3xl overflow-hidden
+                bg-gradient-to-br from-fuchsia-950  via-fuchsia-700
+                 to-fuchsia-500
+                 border border-fuchsia-900
+                 flex flex-col items-start
+                 hover:scale-105 hover:contrast-125
+                 transition
+                 col-span-1 p-7  min-h-[180px]
+                 group">
+                    
+                </a>
+
+                <a className="relative rounded-3xl overflow-hidden
+                bg-gradient-to-br from-green-900 via-green-700
+                 to-green-500
+                 border border-green-900
+                 flex flex-col items-start
+                 hover:scale-105 hover:contrast-125
+                 transition
+                 col-span-1 p-7  min-h-[180px]
+                 group">
+                   
+                </a>
+            </div>
+    )
+}
+export default Pokemones
