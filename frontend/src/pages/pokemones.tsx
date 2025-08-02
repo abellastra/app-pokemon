7ddbd41c7178c8b615ff4024d1b9b8db63c57874
Alexander Armua <alexanderarmua1@gmail.com>
2025-08-02 15:26:15 -0300

    Created vscode folder and configured auto-format

--- frontend/src/pages/pokemones.tsx
+++ frontend/src/pages/pokemones.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import Tarjeta from "../components/tarjetaPokemon";
+import { useEffect, useState } from 'react';
+import Tarjeta from '../components/tarjetaPokemon';
 
 type Pokemon = {
   name: string;
@@ -8,41 +8,40 @@ type Pokemon = {
   foto: string;
 };
 
-
 function Pokemones() {
-const [listaPokemones, setListaPokemones] = useState<Pokemon[]>([])
-    useEffect(() => {
-        pedirDatosPokemones();
-    }, [])
-    const pedirDatosPokemones = async () => {
-        const response = await fetch("http://localhost:3000/pokemones", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-        if (response.ok) {
-            const data = await response.json();
-            setListaPokemones(data)
-            console.log(data);
-        }
-    };
+  const [listaPokemones, setListaPokemones] = useState<Pokemon[]>([]);
+  useEffect(() => {
+    pedirDatosPokemones();
+  }, []);
+  const pedirDatosPokemones = async () => {
+    const response = await fetch('http://localhost:3000/pokemones', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+      const data = await response.json();
+      setListaPokemones(data);
+      console.log(data);
+    }
+  };
 
-
-
-    return (
-        <section id="tajetas" className="py-4 px-4 pb-16 max-w-xl mx-auto min-h-dvh mt-14 flex flex-col items-center ">
-            { listaPokemones.map((pokemon) =>
-                    <Tarjeta
-                    key={pokemon.name}
-                    name={pokemon.name}
-                    habilidades={pokemon.habilidades}
-                    ataques={pokemon.ataques}
-                    foto={pokemon.foto}
-                    />
-            )}
-            
-        </section>
-    )
+  return (
+    <section
+      id='tajetas'
+      className='py-4 px-4 pb-16 max-w-xl mx-auto min-h-dvh mt-14 flex flex-col items-center '
+    >
+      {listaPokemones.map(pokemon => (
+        <Tarjeta
+          key={pokemon.name}
+          name={pokemon.name}
+          habilidades={pokemon.habilidades}
+          ataques={pokemon.ataques}
+          foto={pokemon.foto}
+        />
+      ))}
+    </section>
+  );
 }
-export default Pokemones
+export default Pokemones;
