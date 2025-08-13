c1110374f1c33cd5eafe2f82bdc57ec929205781
gianella lastra <gianellalastra4@gmail.com>
2025-08-13 00:14:23 -0300

    Separé la lógica de paginación en su propio componente y agregué una función para calcular el rango de páginas a mostrar.

--- frontend/src/pages/pokemones.tsx
+++ frontend/src/pages/pokemones.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import Tarjeta from '../components/tarjetaPokemon';
+import Pagination from '../components/pagination';
 
 type Pokemon = {
   name: string;
@@ -12,19 +13,14 @@ type Pokemon = {
 function Pokemones() {
   const [listaPokemones, setListaPokemones] = useState<Pokemon[]>([]);
   const [registros, setRegistros] = useState(0);
-  const [items, setItems] = useState<JSX.Element[]>([]);
   const [paginaActual, setPaginaActual] = useState(1);
-  const [rangoPagina, setRangoPagina] = useState(0);
-  const paginasPorRango = 10;
   const limite = 20;
-
-  // useEffect(() => {
-  //   pedirDatosPokemones();
-  // }, []);
+  const totalPag = Math.ceil(registros / limite);
+ 
   useEffect(() => {
     const offset = (paginaActual - 1) * limite;
     pedirDatosPokemones(offset, limite);
-  }, [paginaActual]);
+  }, [paginaActual,totalPag]);
 
   const pedirDatosPokemones = async (offset: number, limite: number) => {
     const response = await fetch(
@@ -43,77 +39,17 @@ function Pokemones() {
     }
   };
 
-  useEffect(() => {
-    const totalPag = Math.ceil(registros / limite);
-    const nuevosItems = [];
-    for (let i = 1; i <= totalPag; i++) {
-      nuevosItems.push(
-        <li className='page-item' key={i}>
-          <a
-            className='page-link'
-            href='#'
-            onClick={e => {
-              e.preventDefault();
-              setPaginaActual(i);
-            }}
-          >
-            {i}
-          </a>
-        </li>
-      );
-    }
-    setItems(nuevosItems);
-  }, [registros]);
+console.log(registros)
+  function cambiarPagina(value : number){
+  setPaginaActual(value)
+  }
 
-  const inicio = rangoPagina * paginasPorRango;
-  const fin = inicio + paginasPorRango;
-  const itemsVisibles = items.slice(inicio, fin);
   return (
     <>
-      <section
-        id='tajetas'
-        className='py-4 px-4 pb-16 max-w-xl mx-auto   flex- items-center '
-      >
-        <div>
-          <nav aria-label='Page navigation example'>
-            <ul className='pagination'>
-              <li className='page-item'>
-                <a className='page-link' href='#' aria-label='Previous'>
-                  <button
-                    onClick={() =>
-                      setRangoPagina(prev => Math.max(0, prev - 1))
-                    }
-                    disabled={rangoPagina === 0}
-                  >
-                    &laquo;
-                  </button>
-                </a>
-              </li>
-              {itemsVisibles}
-              <li className='page-item'>
-                <a className='page-link' href='#' aria-label='Next'>
-                  <button
-                    onClick={() =>
-                      setRangoPagina(prev =>
-                        (prev + 1) * paginasPorRango < items.length
-                          ? prev + 1
-                          : prev
-                      )
-                    }
-                    disabled={
-                      (rangoPagina + 1) * paginasPorRango >= items.length
-                    }
-                  >
-                    &raquo;
-                  </button>
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </section>
+   
+      <Pagination  totalPaginas={totalPag} paginaActual={paginaActual}  siblings={1} cambiar= {cambiarPagina}/>
       <div className='grid grid-cols-4 gap-4 w-[80%]'>
-        {listaPokemones.map((pokemon, index) => (
+        {listaPokemones.map((pokemon) => (
           <Tarjeta
             name={pokemon.name}
             habilidades={pokemon.habilidades}
---
+++ frontend/src/utils/appUtils.tsx
@@ -0,0 +1,35 @@
+// Genera un array de números desde "inicio" hasta "fin"
+const generarRango = (inicio: number, fin: number): number[] => {
+  return Array.from({ length: fin - inicio + 1 }, (_, i) => inicio + i);
+}
+
+function devolverRangoDePaginacion(
+  totalPaginas: number,   
+  paginaActual: number,  
+  hermanos: number        
+) {
+   
+  if (totalPaginas <= 7) {
+    // Si hay pocas páginas, muestro todas
+    return generarRango(1, totalPaginas);
+  }
+
+  if (paginaActual <= 1 + hermanos * 2) {
+    // Cerca del inicio
+    const rangoIzquierda = generarRango(1, 3 + hermanos * 2);
+    return [...rangoIzquierda, "...", totalPaginas];
+  }
+
+  if (paginaActual >= totalPaginas - hermanos * 2) {
+    // Cerca del final
+    const rangoDerecha = generarRango(totalPaginas - (2 + hermanos * 2), totalPaginas);
+    return [1, "...", ...rangoDerecha];
+  }
+
+  // En el medio
+  const rangoMedio = generarRango(paginaActual - hermanos, paginaActual + hermanos);
+  return [1, "...", ...rangoMedio, "...", totalPaginas];
+}
+
+
+export default devolverRangoDePaginacion;
