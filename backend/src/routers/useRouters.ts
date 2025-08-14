8acd9ef86ab0a68e130c7125dfa13471dc0d7eaf
gianella lastra <gianellalastra4@gmail.com>
2025-08-14 11:29:29 -0300

    implemento la validacion para el correo y contraseña del usuariocon la libreria express-validator

--- backend/src/routers/useRouters.ts
+++ backend/src/routers/useRouters.ts
@@ -1,7 +1,11 @@
 
 import express from "express";
 import { getPokemones } from '../controllers/pokemones';
-import{ pokemonName } from "../controllers/pokemonName";
+import { pokemonName } from "../controllers/pokemonName";
+import { Login } from "../controllers/login";
+import { body} from "express-validator";
+import { validarCampos } from "../middleware/validacionDeCampos";
+
 
 
 const router = express.Router();
@@ -9,5 +13,16 @@ const router = express.Router();
 router.get('/pokemones', getPokemones);
 
 router.get('/pokemon/:name', pokemonName)
+
+router.post('/login-user', [
+    body("correo")
+        .notEmpty().withMessage("El correo ingresado no puede estar vacio.")
+        .isEmail().withMessage("La direccion de correo debe ser valida."),
+    body("contraseña")
+        .notEmpty().withMessage("La contraseña ingresada no puede estar vacia.")
+        .isLength({ min: 6 }).withMessage("La contraseña debe tener al menos 6 caracteres"),
+        validarCampos
+],
+ Login)
 
 export default router;
