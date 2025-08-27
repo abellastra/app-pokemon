2564f42adce89f634e2ff851263ccbf039925814
gianella lastra <gianellalastra4@gmail.com>
2025-08-27 13:59:51 -0300

    agregue el formulario de registro para crear una cuenta y la logica para asegurarse de que el user no esta registrado

--- backend/src/routers/useRouters.ts
+++ backend/src/routers/useRouters.ts
@@ -5,6 +5,7 @@ import { pokemonName } from "../controllers/pokemonName";
 import { Login } from "../controllers/login";
 import { body} from "express-validator";
 import { validarCampos } from "../middleware/validacionDeCampos";
+import { crear } from "../controllers/crearUsers";
 
 import { filtersDb } from "../controllers/filters";
 
@@ -16,15 +17,26 @@ router.get('/pokemones', getPokemones);
 router.get('/pokemon/:name', pokemonName)
 
 router.post('/login-user', [
-    body("correo")
+    body("email")
         .notEmpty().withMessage("El correo ingresado no puede estar vacio.")
         .isEmail().withMessage("La direccion de correo debe ser valida."),
-    body("contraseña")
+    body("password")
         .notEmpty().withMessage("La contraseña ingresada no puede estar vacia.")
         .isLength({ min: 6 }).withMessage("La contraseña debe tener al menos 6 caracteres"),
         validarCampos
 ],
  Login)
+
+router.post('/crear-user', [
+    body("email")
+        .notEmpty().withMessage("El correo ingresado no puede estar vacio.")
+        .isEmail().withMessage("La direccion de correo debe ser valida."),
+    body("password")
+        .notEmpty().withMessage("La contraseña ingresada no puede estar vacia.")
+        .isLength({ min: 6 }).withMessage("La contraseña debe tener al menos 6 caracteres"),
+        validarCampos
+],
+crear)
 router.get(`/pokemones/generation/:selectedGeneration`, filtersDb);
 
 export default router;
