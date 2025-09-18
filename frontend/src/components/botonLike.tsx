cb6a046786adf108f344e874df9739f113c48a86
Abel <abelastra0@gmail.com>
2025-09-18 17:33:02 -0300

    .

--- frontend/src/components/botonLike.tsx
+++ frontend/src/components/botonLike.tsx
@@ -1,90 +1,86 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback, useEffect } from 'react';
 
 type props = {
-    idPokemon: number,
-    likeInicial: boolean
-    botonVisible:boolean
-}
-const Like = ({ idPokemon, likeInicial,botonVisible }: props) => {
-    const [likePhoto, setLike] = useState<boolean>(likeInicial)
-    
-    useEffect(() => {
-        setLike(likeInicial);
-    }, [likeInicial]);
+  idPokemon: number;
+  likeInicial: boolean;
+  botonVisible: boolean;
+};
+const Like = ({ idPokemon, likeInicial, botonVisible }: props) => {
+  const [likePhoto, setLike] = useState<boolean>(likeInicial);
 
+  useEffect(() => {
+    setLike(likeInicial);
+  }, [likeInicial]);
 
-    const updateLike = useCallback(async (nuevoValor: boolean) => {
-        try {
-            const respuesta = await fetch('http://localhost:3000/like-pokemon', {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    statusPhoto: nuevoValor,
-                    idPokemon: idPokemon
-                }),
-                credentials: 'include',
-            })
+  const updateLike = useCallback(
+    async (nuevoValor: boolean) => {
+      try {
+        const respuesta = await fetch('http://localhost:3000/like-pokemon', {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            statusPhoto: nuevoValor,
+            idPokemon: idPokemon,
+          }),
+          credentials: 'include',
+        });
 
-            const resultado = await respuesta.json();
-            if (!resultado.ok) {
-                throw new Error('algo ocurrio')
-            }
-
-        } catch (error) {
-            console.error("Error al actualizar like:", error);
+        const resultado = await respuesta.json();
+        if (!resultado.ok) {
+          throw new Error('algo ocurrio');
         }
-    }, [idPokemon])
-    const handleLike = () => {
-        const nuevoValor = !likePhoto;
-        setLike(nuevoValor)
-        updateLike(nuevoValor)
-    }
+      } catch (error) {
+        console.error('Error al actualizar like:', error);
+      }
+    },
+    [idPokemon]
+  );
+  const handleLike = () => {
+    const nuevoValor = !likePhoto;
+    setLike(nuevoValor);
+    updateLike(nuevoValor);
+  };
 
-
-
-    return (
-      <>
-        {botonVisible && (
-          <button
-            className='focus:outline-none'
-            onClick={() => handleLike()}
-          >
-            {likePhoto ? (
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='red'
-                viewBox='0 0 24 24'
-                strokeWidth={1.5}
-                stroke='red'
-                className='size-8 transition-transform duration-200 ease-in-out transform scale-110 '
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z'
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth={1.5}
-                stroke='currentColor'
-                className='size-8 transition-transform duration-200 ease-in-out hover:scale-110 '
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z'
-                />
-              </svg>
-            )}
-          </button>
-        )}
-      </>
-    );
-}
+  return (
+    <>
+      {botonVisible && (
+        <button className='focus:outline-none' onClick={() => handleLike()}>
+          {likePhoto ? (
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='red'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='red'
+              className='size-8 transition-transform duration-200 ease-in-out transform scale-110 '
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z'
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='size-8 transition-transform duration-200 ease-in-out hover:scale-110 '
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z'
+              />
+            </svg>
+          )}
+        </button>
+      )}
+    </>
+  );
+};
 export default Like;
