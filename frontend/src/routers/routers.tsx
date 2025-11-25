3f0256955f2609ffaa9dc621de04aca20720ae39
abellastra <abelastra0@gmail.com>
2025-11-25 02:36:50 -0300

    se cambiaron los estilos de registro para que quede igual que login

--- frontend/src/routers/routers.tsx
+++ frontend/src/routers/routers.tsx
@@ -2,22 +2,23 @@ import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/home';
 import Navar from '../pages/navar';
 import Login from '../components/login';
+import { FormularioDeRegistro } from '../components/registro';
 import { AuthPrivider } from '../context/AuthContext';
 import { useLocation } from 'react-router-dom';
 const Rutas = () => {
 
   const location = useLocation();
 
-  const islogin = location.pathname === '/login';
+  const islogin = location.pathname === '/login'|| '/registro'
   return (
     <AuthPrivider>
-      {/* <Navar ocultarbotones={islogin} />  */}
+      {!islogin && <Navar ocultarbotones={islogin} />}{' '}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/registro' element={<FormularioDeRegistro/>} />
       </Routes>
     </AuthPrivider>
-
   );
 };
 export default Rutas;
